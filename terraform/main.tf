72682efa28536ad82f08c033148fecdec3f9d4c7
Manuel Patrone <po75558@gmail.com>
2025-02-15 15:16:09 +0000

    final commit

---
+++ terraform/main.tf
@@ -0,0 +1,96 @@
+resource "aws_vpc" "mp_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "mp_public_subnet" {
+  vpc_id                  = aws_vpc.mp_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public"
+  }
+}
+
+resource "aws_internet_gateway" "mp_internet_gateway" {
+  vpc_id = aws_vpc.mp_vpc.id
+
+  tags = {
+    Name = "mp_igw"
+  }
+}
+
+resource "aws_route_table" "mp_public_rt" {
+  vpc_id = aws_vpc.mp_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.mp_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mp_internet_gateway.id
+}
+
+resource "aws_route_table_association" "mp_public_assoc" {
+  subnet_id      = aws_subnet.mp_public_subnet.id
+  route_table_id = aws_route_table.mp_public_rt.id
+}
+
+resource "aws_security_group" "mp_sg" {
+  name        = "public_sg"
+  description = "public security group"
+  vpc_id      = aws_vpc.mp_vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "mp_auth" {
+  key_name   = "mpkey"
+  public_key = file("../mp_key.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mp_auth.id
+  vpc_security_group_ids = [aws_security_group.mp_sg.id]
+  subnet_id              = aws_subnet.mp_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname = self.public_ip,
+      user     = "ubuntu",
+    identityfile = "../mp_key" })
+    interpreter = var.host_os == "windows" ? ["Powershell", "-Command"] : ["bash", "-c"]
+  }
+}
